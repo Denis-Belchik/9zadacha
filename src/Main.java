1b16afda78216b8e75e77e44e17bb5896659370c
Denis-Belchik <denis1989g@mail.ru>
2023-11-07 12:22:41 +0400

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            throw new MyException("Ойой");
+        } catch(MyException e) {
+            System.out.print(e.getMessage());
+        }
     }
 }
